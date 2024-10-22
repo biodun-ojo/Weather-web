afc2614273f4016ca4f0f08f9364aa121267ce91
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-10-22 14:12:32 +0100

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 //weatther app
-
+//hi
 const weatherForm = document.querySelector('.weatherForm')
 const cityInput = document.querySelector('.cityInput')
 const card = document.querySelector('.cardz')
